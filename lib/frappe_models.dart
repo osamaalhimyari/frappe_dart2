e94fec475a94220330ca0f310becd9f82c974cb8
osama <om179999@gmail.com>
2026-02-26 15:42:48 +0300

    third party

---
+++ lib/frappe_models.dart
@@ -0,0 +1,30 @@
+library;
+
+/// =======================
+/// Desk
+/// =======================
+export 'package:frappe_dart/src/models/desk_sidebar_items_response/desk_message.dart';
+export 'package:frappe_dart/src/models/desk_sidebar_items_response/desk_sidebar_items_response.dart';
+
+/// =======================
+/// Desktop Page
+/// =======================
+export 'package:frappe_dart/src/models/desktop_page_response/desktop_page_response.dart';
+export 'package:frappe_dart/src/models/desktop_page_response/card_item.dart';
+
+/// =======================
+/// Charts & Shortcuts
+/// =======================
+export 'package:frappe_dart/src/models/desktop_page_response/chart_item.dart';
+export 'package:frappe_dart/src/models/desktop_page_response/shortcut_item.dart';
+
+/// =======================
+/// Common / Requests
+/// =======================
+export 'package:frappe_dart/src/models/desktop_page_request.dart';
+export 'package:frappe_dart/src/models/get_doctype_response/doc.dart';
+export 'package:frappe_dart/src/models/get_doctype_response/field.dart';
+export 'package:frappe_dart/src/models/get_doctype_response/link.dart';
+export 'package:frappe_dart/src/models/search_link_response/message.dart';
+export 'package:frappe_dart/src/models/get_doctype_response/dashboard.dart';
+export 'package:frappe_dart/src/models/get_doctype_response/transaction.dart';
