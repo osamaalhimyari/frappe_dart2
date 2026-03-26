4b9d9bc07305c0de954dcd78121e0df30bd93add
osama <om179999@gmail.com>
2026-03-26 13:46:26 +0300

    test

--- lib/src/models/get_doc_response/doc.dart
+++ lib/src/models/get_doc_response/doc.dart
@@ -83,7 +83,7 @@ class Doc {
         modified: data['modified'] as String?,
         modifiedBy: data['modified_by'] as String?,
         docstatus: data['docstatus'] as int?,
-        idx: data['idx'] as int?,
+        idx: data['idx'] as String?,
         enabled: data['enabled'] as int?,
         email: data['email'] as String?,
         firstName: data['first_name'] as String?,
@@ -167,7 +167,7 @@ class Doc {
   String? modified;
   String? modifiedBy;
   int? docstatus;
-  int? idx;
+  String? idx;
   int? enabled;
   String? email;
   String? firstName;
